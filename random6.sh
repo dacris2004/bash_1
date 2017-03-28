4c4af187e91a6c8955e48ded6385fdefe48cfcba
Daniel Catalin Cristea <dri@dri.winsys.ro>
2017-03-28 16:17:50 +0300

    random6.sh ok added

--- random6.sh
+++ random6.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 i=0
-while [ $i -lt 6 ]
-do 
-	n=$((RANDOM % 49)) 
-	i=$[$i+1]
-	echo -n $[$n+1]\ \  
+arr=(${array[6]})
+
+while  [ $i -lt 6 ] && [ ${#arr[@]} -lt 6 ]
+do
+        n=$((RANDOM % 49))
+        n=$[$n+1]
+        if [[ " ${arr[@]} " =~ " ${n} " ]]; then
+                continue
+        fi
+        arr[$i]=$n
+        i=$[$i+1]
+        echo -n $n\ \
 done
 
